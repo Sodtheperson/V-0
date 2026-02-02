7c24def54625c2bb90c6a9b52d47f0506252ea9f
Sodtheperson <sidramesh2010@gmail.com>
2026-02-02 15:17:11 +0000

    Added a small blurb on what to do for the platform class

--- src/PlatformClass.py
+++ src/PlatformClass.py
@@ -7,6 +7,19 @@ add the image to the platform
 figure out how to store states
 implement class into Constants
 '''
+
+# Minor thing: 
+# https://www.geeksforgeeks.org/python/pygame-creating-sprites/
+# this is just an example of sprite classes, but also
+# making a sprite also creates a rect object for it
+# so we could use the sprite's rect instead of making one here
+# and since sprites can have images, that would solve the image issue too
+# plus it would also still be a platform class we can index in the floors list,
+# so we can check states on it 
+
+# TLDR: use sprites instead of making new rects since it solves image + rect and keeps states.
+
+
 class Platform(pygame.Rect):
     def __init__ (self, left, top, width, height, state):
         super().__init__(left, top, width, height)
