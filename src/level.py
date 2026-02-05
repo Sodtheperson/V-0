01b941b491a4ffe2fba3c4224e36b101424f13dd
Siddharth Ramesh <sidramesh2010@gmail.com>
2026-02-04 21:06:14 -0500

    added a 'u' platform

--- src/level.py
+++ src/level.py
@@ -7,6 +7,7 @@ test_level: pygame.sprite.Group = pygame.sprite.Group()
 test_level.add(
     Platform((  0, 500), ( 50, 50, 50), width=1000, height=  50, state='s'),
     Platform((200, 400), (100, 50, 30), width= 200, height=  30, state='p'),
+    Platform((300, 300), (200, 50, 30), width= 200, height=  30, state='u'),
     Platform((100, 450), (  0, 82, 85), width= 200, height=  30, state='p'),
     Platform((500, 300), ( 52,200, 48), width= 150, height=  30, state='s')
 )
